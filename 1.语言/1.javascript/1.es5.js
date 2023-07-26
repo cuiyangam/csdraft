c3f8beffd85d404afc4da47a7b0016ba9fab3c57
cuiyangam <cuiyangam@gmail.com>
2023-07-26 08:55:58 +0800

    esx

--- 1.语言/1.javascript/1.es5.js
+++ 1.语言/1.javascript/1.es5.js
@@ -1,36 +1,34 @@
+// https://zh.javascript.info/
+
 // 1.Array
-[0,1,2,3,4].slice(1,3);      // [1, 2]
-[0,1,2,3,4].slice(0);        // [0, 1, 2, 3, 4]
-[0,1,2,3,4].splice(1,2,5);   // [1, 2]  原数组为[0, 5, 3, 4]
-[0,1,1,2,3,4].indexOf(1,2);  // 2
-[0,1,1,2,3,4].lastIndexOf(1,1);// 1
+[0, 1, 2, 3, 4].slice(1, 3);      // [1, 2]
+[0, 1, 2, 3, 4].slice(0);         // [0, 1, 2, 3, 4]
+[0, 1, 2, 3, 4].splice(1, 2, 5);  // [1, 2]  原数组为[0, 5, 3, 4]
+[0, 1, 1, 2, 3, 4].indexOf(1, 2); // 2
+[0, 1, 1, 2, 3, 4].lastIndexOf(1, 1); // 1
 
 // 2.String
-String.fromCharCode(104, 97,97);
-'JavaScript'.slice(1, 4) // "ava"
-'JavaScript'.slice(2, 1) // ""
+String.fromCharCode(97, 118, 97); // "ava"  
+'JavaScript'.slice(1, 4); // "ava"
+'JavaScript'.slice(2, 1); // ""
 
-'JavaScript'.substring(1, 4) // "ava"
-'JavaScript'.substring(2, 1) // "a"
+'JavaScript'.substring(1, 4); // "ava"
+'JavaScript'.substring(2, 1); // "a"
 
-'JavaScript'.substr(1, 4) // "avaS"
-'JavaScript'.substr(2, 1) // "v"
+'cat, bat, sat, fat'.match('at'); // ["at", index: 1, input: "cat, bat, sat, fat"]
+'cat, bat, sat, fat'.search('at'); // 1
 
-'cat, bat, sat, fat'.match('at') //  ["at", index: 1, input: "cat, bat, sat, fat"]
+'aaa'.replace('a', 'b'); // "baa"
+'aaa'.replace(/a/, 'b'); // "baa"
+'aaa'.replace(/a/g, 'b'); // "bbb"
 
-'cat, bat, sat, fat'.search('at') // 1
-
-'aaa'.replace('a', 'b') // "baa"
-'aaa'.replace(/a/, 'b') // "baa"
-'aaa'.replace(/a/g, 'b') // "bbb"
-
-'1,2,3'.split(",");
+'1,2,3'.split(","); // [1,2,3]
 
 // 3.Math
 Math.ceil();
 Math.floor();
 Math.round();
-Math.abs(); 
+Math.abs();
 
 // 4.Date
 (new Date()).getFullYear();
@@ -39,12 +37,11 @@ Math.abs();
 
 // 5.Number
 (10).toString(16); // "a"
-
-(10).toFixed(2) // "10.00"
+(10).toFixed(2); // "10.00"
 
 // 6.Function
-let fn = new Function('a','b','return a+b');
-console.log(fn(1,3));
+let fn = new Function('a', 'b', 'return a+b');
+console.log(fn(1, 3));
 
 Function.prototype.bind();
 Function.prototype.call();
@@ -60,23 +57,28 @@ var a = ['is enumerable'];
 a.propertyIsEnumerable(0);          // 返回 true
 a.propertyIsEnumerable('length');   // 返回 false
 
-console.log(Object.keys(a));
-console.log(Object.getOwnPropertyNames(a));
+console.log(Object.keys(a)); // [0]
+console.log(Object.getOwnPropertyNames(a)); // ['0', 'length']
 
-Object.defineProperty();
-Object.defineProperties();
-
-let s = {
-  w : 12,
-}
-s.q = s;
-let str = JSON.stringify(s); // Uncaught TypeError: Converting circular structure to JSON
+let user = {
+  name: "John"
+};
+let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+Object.defineProperty(user, "name", {
+  value: "John",
+  writable: false,
+  enumerable: false,
+  configurable: false
+});
+Object.defineProperties(user, {
+  name: { value: "John", writable: false },
+  surname: { value: "Smith", writable: false }
+});
 
 // 8.RegExp
 let regex = /^[a-zA-Z]+[0-9]*\W?_$/gi;  // 加载时候解释执行(when the script is loaded.)
 let regex2 = new RegExp(/^[a-zA-Z]+[0-9]*\W?_$/, "gi");  // 运行时候解释执行
-regex.test();
-regex.exec();
+regex.test(); // 正则表达式是否与指定的字符是否串匹配
 function replacer(match, p1, p2, p3, offset, string) {
   // p1 is nondigits, p2 digits, and p3 non-alphanumerics
   // abc12345#$*% abc 12345 #$*% 0 abc12345#$*%
@@ -85,12 +87,13 @@ function replacer(match, p1, p2, p3, offset, string) {
 }
 var newString = 'abc12345#$*%'.replace(/([^\d]*)(\d*)([^\w]*)/, replacer);
 console.log(newString);  // abc - 12345 - #$*%
+
 // 正则表达式中字符的含义
 // 1.字符类别（Character Classes）
 // . \d \D \w \W \s \S \t \r \n \v \f [\b] \0 \xhh \uhhhh
 
 // 2.字符集合（Character Sets）
-// [xyz] [^xyz]
+// [xyz] [^xyz] ^表示非
 
 // 3.边界（Boundaries）
 // ^ $ \b \B
